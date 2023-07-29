9879a53719d45c1d0e9b9e73c02ce32762eaaa66
Chuyue Zhang <70104294+IronDumpling@users.noreply.github.com>
2023-07-29 17:11:49 -0400

    Buttons Work

--- Build/JavaScript/buttons.js
+++ Build/JavaScript/buttons.js
@@ -1,17 +1,30 @@
 // Function to load the Unity WebGL build
 function loadUnity() {
-    var unityContainer = document.querySelector('#unity-container');
-    var unityInstance = UnityLoader.instantiate(unityContainer, 'Build/_Build.json');
+    var myGameInstance = null;
+    var loaderUrl = buildUrl + "/JavaScript/unity.loader.js";
+    var script = document.createElement("script");
+    script.src = loaderUrl;
+    script.onload = () => {
+        createUnityInstance(canvas, config, (progress) => { }).then((unityInstance) => {
+            console.log('Create My Game Instance');
+            myGameInstance = unityInstance;
+        })
+    }
+
+    document.body.appendChild(script);
 
     // Add event listeners to the buttons
     document.getElementById('btnX').addEventListener('click', function() {
-        unityInstance.SendMessage('Main Camera', 'RotateToAxisX');
+        console.log('Click Button X');
+        myGameInstance.SendMessage('Main Camera', 'RotateToAxisX');
     });
     document.getElementById('btnY').addEventListener('click', function() {
-        unityInstance.SendMessage('Main Camera', 'RotateToAxisY');
+        console.log('Click Button Y');
+        myGameInstance.SendMessage('Main Camera', 'RotateToAxisY');
     });
     document.getElementById('btnZ').addEventListener('click', function() {
-        unityInstance.SendMessage('Main Camera', 'RotateToAxisZ');
+        console.log('Click Button Z');
+        myGameInstance.SendMessage('Main Camera', 'RotateToAxisZ');
     });
 }
 
