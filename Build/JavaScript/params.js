b5ae7c3ad318efd73255fb55dc9bbb634b211f80
Chuyue Zhang <70104294+IronDumpling@users.noreply.github.com>
2023-08-08 21:28:35 -0400

    Browser View Work

--- Build/JavaScript/params.js
+++ Build/JavaScript/params.js
@@ -2,82 +2,24 @@
 const WAVETYPE = {
      INVALID: 0,
      PARALLEL: 1,
-     POINT: 2
+     POINT: 2,
 };
 
-// const Theta = {
-//      _value: 0,
-//      listeners: [],
-//      get value(){
-//           return this._value;
-//      },
-//      set value(newValue){
-//           this._value = newValue;
-//           this.notifyListeners();
-//      },
-//      addListener(listener) {
-//           this.listeners.push(listener);
-//      },
-//      notifyListeners() {
-//           this.listeners.forEach(listener => {
-//                listener(this._value);
-//           });
-//      }
-// };
-
-// Define the WaveParams class
-class WaveParams {
-     constructor() {
-          // Initialize properties with default values
-          this.Type = WAVETYPE.INVALID;
-          this.Eox = 0;
-          this.Eoy = 0;
-          this.W = 0;
-          this.K = 0;
-          this.N = 0;
-          this.Theta = 0;
-          this.Phi = 0;
-     }
-}
+const ParamDict = {
+     0: "eox",
+     1: "eoy",
+     2: "w",
+     3: "k",
+     4: "n",
+     5: "theta",
+     6: "phi",
+};
 
 let params = new WaveParams();
 
 function waveParams(){
      // Get Value from Unity
      //gameInstance?.SendMessage("WaveLine", "SendParamsToWeb");
-
-     // Set Value to Unity
-     // Add event listeners to input fields for user modification
-     document.getElementById("eox").addEventListener("input", function() {
-          params.Eox = parseFloat(this.value);
-     });
-
-     document.getElementById("eoy").addEventListener("input", function() {
-          params.Eoy = parseFloat(this.value);
-     });
-
-     document.getElementById("w").addEventListener("input", function() {
-          params.W = parseFloat(this.value);
-     });
-
-     document.getElementById("k").addEventListener("input", function() {
-          params.K = parseFloat(this.value);
-     });
-
-     document.getElementById("n").addEventListener("input", function() {
-          params.N = parseFloat(this.value);
-     });
-
-     document.getElementById("theta").addEventListener("input", function() {
-          console.log("Theta input changes to " + params.Theta);
-          params.Theta = parseFloat(this.value);
-     });
-
-     document.getElementById("phi").addEventListener("input", function() {
-          console.log("Phi input changes to " + params.Phi);
-          params.Phi = parseFloat(this.value);
-     });
-
      genTypeOption();
      genSlider();
 }
@@ -114,11 +56,5 @@ function genSlider(){
                console.log("Input value changes to " + this.value);
                output.textContent = this.value;
           });
-
-          // params.Theta.addListener(function (){
-          //      console.log("Params changes to " + this.value);
-          //      output.textContent = this.value;
-          //      input.value = this.value;
-          // });
      });
 }
