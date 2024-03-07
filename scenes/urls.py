492e74a4252dbd796591b2b47260f029f10ef6f2
Chuyue Zhang <70104294+IronDumpling@users.noreply.github.com>
2024-03-06 22:20:56 -0500

    Delete scenes directory

--- scenes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import views
-from django.urls import path, include
-
-app_name = "scenes"
-
-urlpatterns = [
-    path('', views.SceneListView.as_view(), name='scene-list'),
-]
